a7947052e4cc5ec7515afd44325245864102f068
Ari Bowe <44865080+AriBowe@users.noreply.github.com>
2019-10-21 10:19:10 +1000

    Initial upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function toggleOpen() {
+    document.getElementById("case_study_links").classList.toggle("closed");
+    document.getElementById("open_arrow").classList.toggle("right")
+    document.getElementById("open_arrow").classList.toggle("down")
+}
